b951bc676eb01790e91c99e2b2e3eef0c358da8f
Selwin Ong <selwin.ong@gmail.com>
2012-05-02 23:31:00 +0700

    Updated license classifier

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP',
