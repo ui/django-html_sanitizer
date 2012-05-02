ea34399675a7fc80c12fc8bc184df4ec1d2b908f
Selwin Ong <selwin.ong@gmail.com>
2012-05-02 19:44:44 +0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='django-html_sanitizer',
+    version='0.1.0',
+    author='Selwin Ong',
+    author_email='selwin.ong@gmail.com',
+    packages=['sanitizer'],
+    url='https://github.com/ui/django-html_sanitizer',
+    license='MIT',
+    description='Provides a set of HTML cleaning utilities for django models, forms and templates.',
+    long_description=open('README.rst').read(),
+    zip_safe=False,
+    include_package_data=True,
+    package_data = { '': ['README.rst'] },
+    install_requires=['django', 'bleach'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Markup :: HTML',
+    ]
+)
